6e951a24d3e87585eec13ff3dd09613ccf928323
Shea Levy <shea@shealevy.com>
2018-04-09 12:23:53 -0400

    Export BOOST_INCLUDE_DIR in nix-cflags

--- extra-builtins.cc
+++ extra-builtins.cc
@@ -72,9 +72,10 @@ static RegisterPrimOp rp1("__extraBuiltins", 0,
 static void cflags(EvalState & state, const Pos & _pos,
     Value ** _args, Value & v)
 {
-    state.mkAttrs(v, 2);
+    state.mkAttrs(v, 3);
     mkStringNoCopy(*state.allocAttr(v, state.symbols.create("NIX_INCLUDE_DIRS")), NIX_INCLUDE_DIRS);
     mkStringNoCopy(*state.allocAttr(v, state.symbols.create("NIX_CFLAGS_OTHER")), NIX_CFLAGS_OTHER);
+    mkStringNoCopy(*state.allocAttr(v, state.symbols.create("BOOST_INCLUDE_DIR")), BOOST_INCLUDE_DIR);
 }
 
 static RegisterPrimOp rp2("__nix-cflags", 0,
