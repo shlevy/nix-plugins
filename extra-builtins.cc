524c6c29e4e340ec0c2440b878507983339b65df
David Guibert <david.guibert@gmail.com>
2021-01-03 17:05:47 +0100

    mkStringNoCopy -> mkString

--- extra-builtins.cc
+++ extra-builtins.cc
@@ -69,9 +69,9 @@ static void cflags(EvalState & state, const Pos & _pos,
     Value ** _args, Value & v)
 {
     state.mkAttrs(v, 3);
-    mkStringNoCopy(*state.allocAttr(v, state.symbols.create("NIX_INCLUDE_DIRS")), NIX_INCLUDE_DIRS);
-    mkStringNoCopy(*state.allocAttr(v, state.symbols.create("NIX_CFLAGS_OTHER")), NIX_CFLAGS_OTHER);
-    mkStringNoCopy(*state.allocAttr(v, state.symbols.create("BOOST_INCLUDE_DIR")), BOOST_INCLUDE_DIR);
+    mkString(*state.allocAttr(v, state.symbols.create("NIX_INCLUDE_DIRS")), NIX_INCLUDE_DIRS);
+    mkString(*state.allocAttr(v, state.symbols.create("NIX_CFLAGS_OTHER")), NIX_CFLAGS_OTHER);
+    mkString(*state.allocAttr(v, state.symbols.create("BOOST_INCLUDE_DIR")), BOOST_INCLUDE_DIR);
 }
 
 static RegisterPrimOp rp2("__nix-cflags", 0,
